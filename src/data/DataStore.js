11def21b435cee27bd5368f41aedd3e57d77f3c3
paulius <paulius@2simple.com>
2020-03-20 22:22:03 +0000

    add common reducer, which combines multiple reducers and passes state along with parameters

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,4 +1,6 @@
 import {createStore} from 'redux';
 import {ShopReducer} from "./ShopReducers";
+import {CartReducer} from "./CartReducers";
+import {CommonReducer} from "./CommonReducer";
 
-export const SportsStoreDataStore = createStore(ShopReducer);
+export const SportsStoreDataStore = createStore(CommonReducer(ShopReducer, CartReducer));
